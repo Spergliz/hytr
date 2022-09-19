2472a8b09550d430b9475437e266754db23ce572
liz <97650879+Spergliz@users.noreply.github.com>
2022-09-18 22:06:58 -0600

    h

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Triangle Perimeter Assignment Start Code
 //input
-let xA = document.getElementById("Ax")
-let yA = document.getElementById("Ay")
+let xA = document.getElementById("Ax").Value;
+let yA = document.getElementById("Ay").Value;
+let
 
 //trust the process
 
