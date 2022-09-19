b90dde91cb64b590fb5f1381c633e540d2179a77
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-09-19 13:40:01 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,19 @@
 //input
 let xA = document.getElementById("Ax").Value;
 let yA = document.getElementById("Ay").Value;
-let
-
+let xB = document.getElementById("Bx").value;
+let yB = document.getElementById("By").value;
+let xC = document.getElementById("cx").value;
+let yC = document.getElementById("cy").value;
 //trust the process
 
 
 //output 
+
+
+//function
+function dist(){
+
+
+    
+}
