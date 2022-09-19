b11229bbcde015c0a361af0c35696f696ac4fdb7
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-09-19 14:47:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 // Triangle Perimeter Assignment Start Code
-document.getElementById("calc-btn").addEventListener("click",dist)
+
 //input 
 
-function a() {
+function bttn() {
     let xA = document.getElementById("Ax").Value;
     let yA = document.getElementById("Ay").Value;
     let xB = document.getElementById("Bx").value;
     let yB = document.getElementById("By").value;
     let xC = document.getElementById("Cx").value;
     let yC = document.getElementById("Cy").value;
-}
+
 
 //trust the process
 
 
 //output 
-
+document.getElementById("AB").innerHTML = dist;
+document.getElementById("BC").innerHTML = dist;
+document.getElementById("AC").innerHTML = dist;
+}
 
 //function
-function dist(xA,yA,xB,yB,xC,yC){
-    let run 
-   let rise 
-
-
+function dist(x1,x2,y1,y2){
+    let rise= x2-x1
+    let run = y2-y1
 }
