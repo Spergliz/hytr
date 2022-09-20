c27e62270898ff5543467bb46f174e48d9a2b485
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-09-20 14:20:49 -0600

    ghv

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 // Triangle Perimeter Assignment Start Code
+document.getElementById("calc-btn").addEventListener("click", bttn);
 
-//input 
+//input
 
 function bttn() {
-    let xA = document.getElementById("Ax").Value;
-    let yA = document.getElementById("Ay").Value;
-    let xB = document.getElementById("Bx").value;
-    let yB = document.getElementById("By").value;
-    let xC = document.getElementById("Cx").value;
-    let yC = document.getElementById("Cy").value;
+  let xA = +document.getElementById("Ax").value;
+  let yA = +document.getElementById("Ay").value;
+  let xB = +document.getElementById("Bx").value;
+  let yB = +document.getElementById("By").value;
+  let xC = +document.getElementById("Cx").value;
+  let yC = +document.getElementById("Cy").value;
 
-
-//trust the process
-let AB = dist(xA,xB,yA,yB)
-let BC = dist(xB,xC,yB,yC)
-let AC = dist(xA,xC,yA,yC)
-//output 
-document.getElementById("AB").innerHTML = AB;
-document.getElementById("BC").innerHTML = BC;
-document.getElementById("AC").innerHTML = AC;
+  console.log(xA,yA,xB,yB,xC,yC)
+  //trust the process
+  let AB = dist(xA, xB, yA, yB);
+  let BC = dist(xB, xC, yB, yC);
+  let AC = dist(xA, xC, yA, yC);
+  //output
+  document.getElementById("AB").innerHTML = AB;
+  document.getElementById("BC").innerHTML = BC;
+  document.getElementById("AC").innerHTML = AC;
 }
 
 //function
-function dist(x1,x2,y1,y2){
-    let rise= x2-x1
-    let run = y2-y1
-    return Math.sqrt(rise ** 2 + run ** 2);
+function dist(x1, x2, y1, y2) {
+  let rise = x2 - x1;
+  let run = y2 - y1;
+  return Math.sqrt(rise ** 2 + run ** 2);
 }
